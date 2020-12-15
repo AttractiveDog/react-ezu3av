6ad1814c3befaa3d1100084318b2ab7b55e98d52
AttractiveDog <vedanshm41@gmail.com>
2020-12-15 12:58:56 +0530

    Initial commit

---
+++ src/spotify.js
@@ -0,0 +1,25 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "https://react-ezu3av.stackblitz.io/";
+const clientId = "e298babc4f434f24bd4c4a969a3e269d";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
